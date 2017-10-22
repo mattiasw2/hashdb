f0093301eb49c49b0f10a94dff0c5f57d0bcfb11
mattias <mattias@spreadsheetconverter.com>
2017-10-22 21:49:16 +0200

    Experimenting on how to use spec for database contents

---
+++ src/clj/hashdb/db/testschema.clj
@@ -0,0 +1,34 @@
+(ns hashdb.db.testschema
+  (:require
+   [clojure.spec.alpha :as s]
+   [clj-time.jdbc]
+   [clojure.java.jdbc :as jdbc]
+   [clojure.java.jdbc :as sql]
+   [hashdb.config :refer [env]]
+   [hashdb.db.core :as cmd])
+  (:import [java.sql
+            BatchUpdateException
+            PreparedStatement]))
+
+
+;; why not use spec, i.e. say that schema should be a spec, whose topmost is a map
+;; Things you want to have indexes has to be typed by a spec. (string? float? doible? int? timestamp?
+;; get-spec reads spec (registry is all)
+
+(s/def :ins/id string?)
+(s/def :ins/version int?)
+
+;; the main of :ins
+(s/def :ins/entity map?)
+
+(s/def :ins/t (s/keys :req [:ins/id :ins/version]))
+
+
+;; (= (s/describe (s/get-spec :ins/id)) 'string?)
+;; => true
+
+;; (= (s/describe (s/get-spec :ins/entity)) 'map?)
+;; => true
+
+;; (= 'keys (first (s/describe (s/get-spec :ins/t))))
+;; true
