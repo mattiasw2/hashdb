a936da23711b3fbcb155c0a9590aadf0b0f69c90
mattias <mattias@spreadsheetconverter.com>
2017-10-23 09:33:06 +0200

    Just create two objects and delete them.

--- test/clj/hashdb/db/commands_test.clj
+++ test/clj/hashdb/db/commands_test.clj
@@ -1,7 +1,7 @@
 (ns hashdb.db.commands-test
   (:require
    [clojure.test :refer :all]
-   [hashdb.db.command :refer :all]
+   [hashdb.db.commands :refer :all]
    [clj-time.jdbc]
    [clojure.java.jdbc :as jdbc]
    [clojure.java.jdbc :as sql]
@@ -22,7 +22,14 @@
 ;;       (is (= 404 (:status response))))))
 
 
-;; (hashdb.db.commands/create {:då "foo"})
-;; (hashdb.db.commands/update (hashdb.db.commands/get "6df4b724-7519-4296-b5b9-021aaaa58033") {:bar "foo"})
-;; (hashdb.db.commands/delete (hashdb.db.commands/get "6df4b724-7519-4296-b5b9-021aaaa58033"))
-;; (clojure.pprint/pprint (hashdb.db.commands/history "6df4b724-7519-4296-b5b9-021aaaa58033"))
+(defn test-all-commands
+  []
+  (let [m1 (hashdb.db.commands/create {:då "foo"})
+        id1 (:id m1)
+        m2 (hashdb.db.commands/create {:bar "rolf"})
+        id2 (:id m2)]
+    (hashdb.db.commands/update (hashdb.db.commands/get id1) {:bar "foo"})
+    (hashdb.db.commands/delete-by-id id2)
+    (hashdb.db.commands/delete (hashdb.db.commands/get id1))
+    (clojure.pprint/pprint (hashdb.db.commands/history id1))
+    (clojure.pprint/pprint (hashdb.db.commands/history id2))))
