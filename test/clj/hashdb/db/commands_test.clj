69ac7bdf71aeb47dc74a0651b77c91e1b1ff1928
mattias <mattias@spreadsheetconverter.com>
2017-10-24 08:24:15 +0200

    Added nil-entity call to sample code.

--- test/clj/hashdb/db/commands_test.clj
+++ test/clj/hashdb/db/commands_test.clj
@@ -24,11 +24,11 @@
 
 (defn test-all-commands
   []
-  (let [m1 (hashdb.db.commands/create {:då "foo"})
+  (let [m1  (hashdb.db.commands/create {:då "foo"})
         id1 (:id m1)
-        m2 (hashdb.db.commands/create {:bar "rolf"})
+        m2  (hashdb.db.commands/create {:bar "rolf"})
         id2 (:id m2)
-        m3 (hashdb.db.commands/create {:gunnar "mattias"})
+        m3  (hashdb.db.commands/create {:gunnar "mattias"})
         id3 (:id m3)]
     (hashdb.db.commands/update (hashdb.db.commands/get id1) {:bar "foo"})
     (hashdb.db.commands/delete-by-id id2)
@@ -38,9 +38,9 @@
     (println "m1")
     (clojure.pprint/pprint (hashdb.db.commands/history id1))
     (println "m2")
-    (clojure.pprint/pprint (hashdb.db.commands/history id2))
+    (clojure.pprint/pprint (hashdb.db.commands/history-short id2))
     (println "m3")
-    (clojure.pprint/pprint (hashdb.db.commands/history id3))))
+    (clojure.pprint/pprint (hashdb.db.commands/history-nil-entity))))
 
 
 ;; bugs:
