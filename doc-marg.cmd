4d358b23369ef2e156483343debf9d648bee753f
mattias <mattias@spreadsheetconverter.com>
2017-11-10 09:31:03 +0100

    etags and generating docs

---
+++ doc-marg.cmd
@@ -0,0 +1,12 @@
+:restart
+rem markdown syntax at https://daringfireball.net/projects/markdown/syntax
+
+rem "call lein marg" works fine
+rem -m seems to create syntax error
+rem call lein marg -m -D -v -n
+rem call lein marg -D "desc" -v "1.0" -n "name"
+rem lein marg -m
+
+call java -jar C:/data4/clojure/marginalia/target/marginalia-0.9.0-standalone.jar -m -r c:/data4/clojure/hashdb/
+pause
+goto :restart
