5c5a4164a0025649e3ba9ddfd4788e24d2cc4c72
mattiasw <mattiasw2@users.noreply.github.com>
2017-11-24 16:20:14 +0100

    v0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mattiasw/hashdb "0.1.0"
+(defproject org.clojars.mattiasw/hashdb "0.1.1"
 
   :description "HASHDB: A simple way of storing clojure maps into mySQL."
   :url "https://github.com/mattiasw2/hashdb"
